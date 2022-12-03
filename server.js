c478ec14fe083612c413683a834a50e66d0af0b6
https://github.com/Lindarosado16 <https://github.com/Lindarosado16>
2022-12-03 12:29:28 +0100

    adelanto

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+
 const app = express();
 
 app.use(cors());
+
+const bodyParser = require('body-parser');
+const { request, response } = require("express");
+app.use (bodyParser.json());
+app.use(bodyParser.urlencoded({ extends: true }));
 
 const welcomeMessage = {
   id: 0,
@@ -18,6 +24,20 @@ const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
+});
+app.get("/messages", function (request, response){
+  response.status(200).json(messages);
+
+});
+app.post("/messages",(request, response)=> {
+const newMessageId = messages.length > 0 
+const newMessage = {
+  id: newMessageId, 
+  ...reques.body
+}
+messages.push(newMessage);
+response.status(201).json(messages);
+
 });
 
 app.listen(3000, () => {
